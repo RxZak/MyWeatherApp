eb2ecabb526135201029204a49cbaadebb367c98
Zakariae ISMAILI <zakariae.ismaili@babelgroup.com>
2023-09-12 09:49:21 +0100

    Displaying hard coded Weather into tableview

---
+++ MyWeatherApp/WeatherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherTableViewCell.swift
+//  MyWeatherApp
+//
+//  Created by zakaria.ismaili on 12/9/2023.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
